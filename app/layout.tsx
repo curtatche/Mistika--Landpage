63212d4c4125f306cb4fcbbb686d5d1a26780eeb
Nice Vargas <eunicelvargas@gmail.com>
2026-03-21 11:27:41 -0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from 'next';
 import { Playfair_Display, Jost } from 'next/font/google';
 import Script from 'next/script';
+import { Analytics } from "@vercel/analytics/next";
 import './globals.css';
 
 const playfair = Playfair_Display({
@@ -53,6 +54,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
           />
         </noscript>
         {children}
+        <Analytics />
       </body>
     </html>
   );
